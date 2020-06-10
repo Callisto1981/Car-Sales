3ee3fba385070c38d34d664c9f3fe9e7a76ec2d8
Wayne <wabbruscatoorr@gmail.com>
2020-06-10 16:32:46 -0400

    updating action file

---
+++ src/actions/actions.js
@@ -0,0 +1,13 @@
+//Action suite
+//action types
+//action creators
+//action objects (actions)
+
+//action types
+export const ADD_FEATURE = "ADD_FEATURE";
+
+//action creator
+export const addAction = () => {
+  //action object
+  return { type: "ADD_FEATURE" };
+};
